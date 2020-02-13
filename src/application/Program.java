fd90ba80c997b71593e9d5a61ae1b60f760de204
Lelton Borges <lelton.borges1@gmail.com>
2020-02-12 22:03:44 -0300

    Creating project and git repository

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+import connections.DB;
+
+public class Program {
+	public static void main(String[] args) {
+		DB.getConnection();
+	}
+
+}
---
+++ src/exceptions/FileExcecption.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class FileExcecption extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public FileExcecption(String msg) {
+		super(msg);
+	}
+	
+}
